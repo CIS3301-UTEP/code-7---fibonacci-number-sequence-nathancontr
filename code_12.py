aa528d674e4903a639c131705d28aa804d542bdf
nathancontr <ncontreras9@miners.utep.edu>
2024-11-02 11:03:06 -0600

    even more tweaking

--- code_12.py
+++ code_12.py
@@ -28,7 +28,7 @@ def get_fibonacci_number_sequence(number):
         #to the list
         #didnt use number-1 here because in testing it would give me
         #the sequence excluding the number I asked for
-        sequence.append(get_fibonacci_number(number - 1))
+        sequence.append(get_fibonacci_number(number))
         return sequence
 
 if __name__ == "__main__":
@@ -38,10 +38,10 @@ if __name__ == "__main__":
 
     print("\nHello! Welcome to the Fibonacci Number Sequence Finder!")
     user_input = int(input("\n Please enter the position of the number in the Fibonacci Sequence you are asking to find: "))
-
+#inserts user input into both functions
     answer = get_fibonacci_number(user_input)
     sequence = get_fibonacci_number_sequence(user_input)
-
+#outputs
     print(f"The Fibonacci number at position {user_input} is: \n{answer}")
     print(f"The Fibonacci sequence to reach position {user_input} is: \n{sequence}")
     
