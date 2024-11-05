21fe7314d512b08593bb69e42f5f9676bc061bf5
nathancontr <ncontreras9@miners.utep.edu>
2024-11-05 12:58:53 -0700

    Update code_12.py

--- code_12.py
+++ code_12.py
@@ -16,16 +16,13 @@ def get_fibonacci_number_sequence(number):
     #in the position, so i return the very same numbers
     elif number == 1:
         return [get_fibonacci_number(0), get_fibonacci_number(1)]
-    #returns the first two numbers in the sequence,
-    #which is 0, and 1, dont need to create a special list code
-    # elif number == 2:
-        # return [get_fibonacci_number(0), get_fibonacci_number(1)]
     #calls back itself, i use number-1 because the first position isnt 1,
     #it is 0
     else:
         sequence = get_fibonacci_number_sequence(number - 1)
         #the append adds the higher and higher sequence numbers
         #to the list
+        
         #didnt use number-1 here because in testing it would give me
         #the sequence excluding the number I asked for
         sequence.append(get_fibonacci_number(number))
